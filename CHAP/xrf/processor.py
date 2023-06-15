0d7e167154b4b1ad574b6494822c666d059096ce
Rachel Farley <rf446@lnx201.classe.cornell.edu>
2023-06-14 20:07:05 -0400

    add: initial structure for ElementConcentrationMapProcessor

--- CHAP/xrf/processor.py
+++ CHAP/xrf/processor.py
@@ -1,3 +1,15 @@
+class ElementConcentrationMapProcessor(Processor):
+    """
+    A processor that takes a fit configuration and returns map of element concentrations.
+    """
+    def process(self,data):
+        """
+        Returns map of element concentrations 
+        """
+        print data
+        return data
+
+        
 if __name__ == '__main__':
     # local modules
     from CHAP.processor import main
