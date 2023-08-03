f5a4bbdb5cc3467b4580703737e2cd0d7a69b0fd
Rachel Farley <rf446@jupyter01.classe.cornell.edu>
2023-08-03 11:42:09 -0400

    housekeeping

--- CHAP/xrf/__init__.py
+++ CHAP/xrf/__init__.py
@@ -1,7 +1,7 @@
 """This subpackage contains 'PipelineItems' unique to XRF data processing workflows.
 """
 # from CHAP.xrf.reader import
-#from CHAP.xrf.processor import
+from CHAP.xrf.processor import ElementConcentrationMapProcessor
 #from CHAP.edd.writer import
 
 #from CHAP.common import
